39154af591b455f1b35288ad7104471287efae8a
Керим Гучигов <forsyte12316gmail.com@MacBook-Air-Kerim.local>
2023-03-05 08:30:48 +0300

    сложил все в одну папку

---
+++ task47.cs
@@ -0,0 +1,46 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] CreateMatrix (int m, int n)
+{
+    double number1 = 0.0;
+    double number2 = 0.0;
+    double number3 = 0.0;
+    double[,] matrix = new double[m,n];
+    for (int i=0; i<m; i++)
+    {
+        for (int j=0; j<n; j++)
+        {
+            number1 = new Random().Next(0,100);
+            number2 = new Random().Next(0,100);
+            number3 = Math.Round(number1/number2, 2);
+            matrix[i,j] = number3;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i=0; i<matrix.GetLength(0); i++)
+    {
+        for (int j=0; j<matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i,j]+" ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+System.Console.WriteLine("Введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix  = CreateMatrix(m,n);
+PrintMatrix(matrix);
---
+++ task50.cs
@@ -0,0 +1,57 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает
+// позиции элемента в двумерном массиве, и возвращает значение
+// этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int[,] CreateMatrix()
+{
+    int[,] matrix = new int[5, 5];
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+
+            matrix[i,j] = new Random().Next(1, 101);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FindNumber (int[,] matrix, int m, int n)
+{
+   
+    if (m-1<matrix.GetLength(0) && n-1<matrix.GetLength(1))
+    {
+        System.Console.WriteLine(matrix[m-1,n-1]);
+    }
+    else
+    {
+        System.Console.WriteLine("Искомого занчения в массиве нет");
+    }
+
+}
+System.Console.WriteLine("Введите искомую строку");
+int m = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите искомый столбец");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = CreateMatrix();
+PrintMatrix(matrix);
+FindNumber(matrix, m, n);
